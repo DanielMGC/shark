9fc7e6c13c5739e299edd663c660864cb0a86f9d
Daniel Moises Gonzalez Clua <daniel.moises.gc@gmail.com>
2025-03-08 00:41:18 -0500

    changes

--- index.js
+++ index.js
@@ -28,9 +28,10 @@ var  nextSplashFrame = function(){
 var nextSharkFrame = function(){
     if(sharkTalking){
         sharkTime++;
-        if(sharkTime == 280){
+        if(sharkTime == 240){
             sharkTalking = false;
             babyTimer = setInterval(nextBabyFrame, 50);
+            $(".div-input-area").removeClass("hidden");
         }
     }
     if(sharkDead ){
@@ -54,8 +55,15 @@ var nextSharkFrame = function(){
         if(sharkFrame == 0 || sharkFrame == 2 || sharkFrame == 3) sharkFrame = 1;
         else sharkFrame = sharkTalking ? 3 : 2;
     } else {
-        if(sharkFrame < 11)
+        if(sharkFrame < 11){
             sharkFrame++;
+        }
+        else {
+            clearInterval(sharkTimer);
+            $(".div-answer").removeClass("display-none");
+            $(".div-text-baby").html("Muito bem, vocês conseguiram! Aqui está a última pista para encontrar o tesouro!");
+            $(".div-text-dica").addClass("display-none");
+        }
     }
 
     var newClass = "f" + sharkFrame.toString();
@@ -84,32 +92,46 @@ $(function() {
     
     var soundShark = document.getElementById("audioShark");
     var soundBaby = document.getElementById("audioBaby");
+    var soundHahaha = document.getElementById("audioHahaha");
+    var soundFinal = document.getElementById("audioFinal");
     $(".div-splash").click(function () {
         if(!sharkTalking && !sharkDead && splashFrame > 10){
             sharkTalking = true;
             sharkTime = 0;
+            soundShark.currentTime = 0;
+            soundBaby.pause();
+            soundHahaha.pause();
             soundShark.play();
             $(".div-text-area").removeClass("hidden");
-            $("#txtPalavra").focus();
         }
     });
 
     $(".div-baby").click(function () {
         if(babyPos<=0){
+            soundHahaha.pause();
+            soundShark.pause();
+            soundBaby.currentTime = 0;
             soundBaby.play();
             $(".div-text-baby").removeClass("hidden");
             $(".div-text-dica").removeClass("hidden");
-            $("#txtPalavra").focus();
         }
     });
 
     $("#btnOk").click(function () {
+        soundHahaha.pause();
+        soundShark.pause();
+        soundBaby.pause();
         var palavra = $("#txtPalavra").val();
         palavra = palavra.toLowerCase();
-        var newText = "BUAHAHAHAHA"
+        var newText = "BUAHAHAHAHA";
         if(palavra == "tsunami"){
+            soundFinal.play();
             newText = "NÃÃÃÃÃÃÃOOOOOOO";
             sharkDead = true;
+            $(".div-input-area").addClass("display-none");
+        } else {
+            soundHahaha.currentTime = 0;
+            soundHahaha.play();
         }
         $(".div-text-area").html(newText);
         $("#txtPalavra").val("");
